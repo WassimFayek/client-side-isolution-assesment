bba08a38b3454c021807070e5d2846befa6b7305
wassim <wassim99f@gmail.com>
2023-12-07 14:13:04 +0200

    create the delete function through axios call and update fetching contacts to be in a separate function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,42 @@ import React, {useEffect, useState} from "react";
 const Home: React.FC = () => {
     const [contacts, setContacts] = useState([]);
 
+    const fetchContacts = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/list-contacts');
+            setContacts(response.data);
+        } catch (error) {
+            console.error('Error fetching contacts:', error);
+        }
+    };
+
     useEffect(() => {
-        axios.get('http://localhost:8000/api/list-contacts')
-            .then(response => setContacts(response.data))
-            .catch(error => console.error('Error fetching contacts:', error));
+        fetchContacts().then();
     }, []);
+
+    const handleDelete = async (id: number) => {
+        try {
+            await axios.delete(`http://localhost:8000/api/delete-contacts/${id}`);
+            await fetchContacts();
+            console.log('Contact deleted successfully');
+        } catch (error) {
+            console.error('Error deleting contact:', error);
+        }
+    };
+
 
     return (
         <>
-        <header className={'flex justify-between items-center p-4 '}>
-            <h1 className={'text-4xl p-6'}>Contact List</h1>
-            <button>Add Contact</button>
+            <header className={'flex justify-between items-center p-4 '}>
+                <h1 className={'text-4xl p-6'}>Contact List</h1>
+                <button>Add Contact</button>
 
-        </header>
-        <div>
-            <Table contacts={contacts} onDelete={() => console.log("delete")} onUpdate={() => {console.log("update")}}/>
-        </div>
+            </header>
+            <div>
+                <Table contacts={contacts} onDelete={handleDelete} onUpdate={() => {
+                    console.log("update")
+                }}/>
+            </div>
         </>
     )
 }
